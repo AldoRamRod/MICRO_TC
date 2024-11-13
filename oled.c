85b9e3233b4a4e77a733e9eb52a830c6640fea3d
AldoRamRod <aldogol1008@gmail.com>
2024-11-12 19:31:31 -0600

    oled.c

--- oled.c
+++ oled.c
@@ -48,7 +48,7 @@ void print(void) {
         for(int i=0; i<sizeof(words)/sizeof(char *); ++i) {
             ssd1306_draw_string(&disp, 25, 25, 2, words[i]);
             ssd1306_show(&disp);
-            sleep_ms(800);
+            sleep_ms(1000);
             ssd1306_clear(&disp);
         }        
     }
